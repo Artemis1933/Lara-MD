a575568d4f5e583a31e967d64abb2fb03de02f51
Artemis1933 <168688859+Artemis1933@users.noreply.github.com>
2024-12-10 00:40:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "yrADhaCC#DNAZLBLkIl1Asr-_jVlFh7WRU_HMwKpNzDzE6Qr1RMo", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "ruYRQBJQ#EYUon3oswpYAWmNP8_XEeYpCaCdPv0QEx7V_E8xfAN4", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
